4e94d586a5cfdbbd14a140ce0777d1d8b1892b58
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-04-06 13:32:20 +0530

    feat: add multiple views

--- core/views.py
+++ core/views.py
@@ -14,3 +14,15 @@ def blog_post(request, slug):
 
 def test_routing(request):
     return HttpResponse(f"<h1>URL Routing is working perfectly!<h1>")
+
+def about_page(request):
+    return HttpResponse("<h1>About Us</h1><p>This is a simple Django learning project.</p>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Page</h1><p>Email: example@gmail.com</p>")
+
+def services_page(request):
+    return HttpResponse("<h1>Our Services</h1><ul><li>Web Development<li><li>AI Integration<li></ul>")
+
+def dynamic_year(request, year):
+    return HttpResponse(f"<h1>Year: {year}</h1><p>This is a dynamic page for {year}</p>")
