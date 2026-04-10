ec346e83a1f4e166df7edec2629b564fdfacd4c2
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-04-10 11:16:48 +0530

    feat: add Django forms basics with ContactForm and validation

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 def home_page(request):
     return render(request, 'core/home.html')
@@ -22,11 +23,23 @@ def blog_post(request, slug):
 def test_routing(request):
     return HttpResponse(f"<h1>URL Routing is working perfectly!<h1>")
 
-def contact_page(request):
-    return HttpResponse("<h1>Contact Page</h1><p>Email: example@gmail.com</p>")
+#def contact_page(request):
+#    return HttpResponse("<h1>Contact Page</h1><p>Email: example@gmail.com</p>")
 
 def services_page(request):
     return HttpResponse("<h1>Our Services</h1><ul><li>Web Development</li><li>AI Integration</li></ul>")
 
 def dynamic_year(request, year):
     return HttpResponse(f"<h1>Year: {year}</h1><p>This is a dynamic page for {year}</p>")
+
+def contact_page(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print("✅ Form submitted successfully!")
+            print(form.cleaned_data)
+            return render(request, 'core/contact_success.html', {'form' : form})
+        
+    else:
+        form = ContactForm()
+        return render(request, 'core/contact_success.html', {'form' : form})
