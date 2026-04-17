12601d0d0927932fbae4cdf169986257a57c3fba
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-04-17 16:39:02 +0530

    feat: implement ForeignKey relationship between Post and Category

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    search_fields = ['name']
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'published_date']
+    list_display = ['title', 'category', 'published_date']
     search_fields = ['title', 'excerpt']
+    list_filter = ['category']
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['name']
+
 class Post(models.Model):
     title = models.CharField(
         max_length=200,
@@ -10,8 +22,15 @@ class Post(models.Model):
         unique=True
     )
     excerpt = models.TextField()
-
     content = models.TextField()
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        null=True,
+        blank=True
+    )
 
     published_date = models.DateTimeField(
         auto_now_add=True
