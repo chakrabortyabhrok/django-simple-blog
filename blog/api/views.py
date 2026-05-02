8dcb7ac0680ca9882c44b5d20c7c26b7e2c4bf97
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-05-02 14:08:07 +0530

    feat: add PUT, PATCH and DELETE endpoints for Post API

--- blog/api/views.py
+++ blog/api/views.py
@@ -19,3 +19,47 @@ class PostListView(APIView):
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
         
+class PostDetailView(APIView):
+    def get_object(self, slug):
+        try:
+            return Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return None
+        
+    def get(self, request, slug):
+        post = self.get_object(slug)
+
+        if post is None:
+            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def put(self, request, slug):
+        post = self.get_object(slug)
+
+        if post is None:
+            return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)        
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def patch(self, request, slug):
+        post = self.get_object(slug)
+
+        serializer = PostSerializer(post, request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def delete(self, request, slug):
+        post = self.get_object(slug)
+
+        if post is None:
+            return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+        post.delete()
+        return Response({'message': "Post deleted successfuly"}, status=status.HTTP_204_NO_CONTENT)
+    
+    
