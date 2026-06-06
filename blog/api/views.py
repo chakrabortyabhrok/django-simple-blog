1072ea869311df16eb8ded4d6ff22058f4916b4c
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-06 10:57:54 +0530

    feat: added filtering feature.

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,9 +4,12 @@ from blog.models import Post
 from blog.serializers import PostSerializer
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'author']
     serializer_class = PostSerializer
 
     lookup_field = 'slug'
--- myfirstproject/settings.py
+++ myfirstproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     'blog',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
