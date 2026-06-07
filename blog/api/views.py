2b4c6e7995a3a4e91ea390b21aaf45e78482a3f8
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-07 10:23:44 +0530

    feat: added pagnition feature.

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,13 +3,21 @@ from rest_framework.response import Response
 from blog.models import Post
 from blog.serializers import PostSerializer
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
+class StandardPagnition(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category', 'author']
     serializer_class = PostSerializer
-
+    pagination_class = StandardPagnition
+    
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'excerpt', 'content']
+    filterset_fields = ['category', 'author']
     lookup_field = 'slug'
